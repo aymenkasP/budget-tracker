c5193fe40761bbf857e163af093a0215b1472af2
aymen <40662254+aymenkasP@users.noreply.github.com>
2021-05-15 09:18:04 +0100

    Update Firestore.js

--- src/Firestore/Firestore.js
+++ src/Firestore/Firestore.js
@@ -3,12 +3,7 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
  const firebaseConfig = {
-    apiKey: "AIzaSyBDJ36J7NdaWbN2T-MURaWgsSTPlinix8g",
-    authDomain: "learning-firestore-ef4a6.firebaseapp.com",
-    projectId: "learning-firestore-ef4a6",
-    storageBucket: "learning-firestore-ef4a6.appspot.com",
-    messagingSenderId: "1017955442766",
-    appId: "1:1017955442766:web:c8ed7e73f89bb4f78d631f"
+  
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
